f127177ca8a9b1d1c6944dedbdabc0efe64da2e0
24-ux <1815565869qq.com>
2026-04-01 13:09:13 +0800

    清空仓库所有文件

--- MDK-ARM/RC26_H7_R1/pid.d
+++
@@ -1,7 +0,0 @@
-rc26_h7_r1\pid.o: ..\user\src\pid.c
-rc26_h7_r1\pid.o: ../user/inc/pid.h
-rc26_h7_r1\pid.o: E:\Keil5\Core\ARM\ARMCC\Bin\..\include\stdint.h
-rc26_h7_r1\pid.o: E:\Keil5\Core\ARM\ARMCC\Bin\..\include\stdbool.h
-rc26_h7_r1\pid.o: E:\Keil5\Core\ARM\ARMCC\Bin\..\include\stdlib.h
-rc26_h7_r1\pid.o: E:\Keil5\Core\ARM\ARMCC\Bin\..\include\string.h
-rc26_h7_r1\pid.o: E:\Keil5\Core\ARM\ARMCC\Bin\..\include\math.h
--- user/src/lift.c
+++
@@ -1,8 +0,0 @@
-#include "lift.h"
-#include "remote_control.h"
-#include "main.h"
-#include "tim.h"
-#include "cmsis_os.h"
-#include <math.h>
-
-
