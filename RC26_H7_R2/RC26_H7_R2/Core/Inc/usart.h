85c1fcaae3df5b31565af201d99ff70aa3c62433
wwwen72 <noreply@github.com>
2026-05-26 19:15:07 +0800

    更换串口5，用串口3（收发）

--- RC26_H7_R2/RC26_H7_R2/Core/Inc/usart.h
+++ RC26_H7_R2/RC26_H7_R2/Core/Inc/usart.h
@@ -32,8 +32,6 @@ extern "C" {
 
 /* USER CODE END Includes */
 
-extern UART_HandleTypeDef huart5;
-
 extern UART_HandleTypeDef huart7;
 
 extern UART_HandleTypeDef huart9;
@@ -42,6 +40,8 @@ extern UART_HandleTypeDef huart1;
 
 extern UART_HandleTypeDef huart2;
 
+extern UART_HandleTypeDef huart3;
+
 extern UART_HandleTypeDef huart10;
 
 /* USER CODE BEGIN Private defines */
@@ -49,11 +49,11 @@ extern DMA_HandleTypeDef hdma_uart9_rx;
 extern DMA_HandleTypeDef hdma_usart2_rx;
 /* USER CODE END Private defines */
 
-void MX_UART5_Init(void);
 void MX_UART7_Init(void);
 void MX_UART9_Init(void);
 void MX_USART1_UART_Init(void);
 void MX_USART2_UART_Init(void);
+void MX_USART3_UART_Init(void);
 void MX_USART10_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
